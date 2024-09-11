a3ece19b1c31b1837f76120b9c93b83205a1ae8f
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2024-09-10 22:31:49 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Users/Index', [
+        'name' => 'João Vitor Gouveia',
+    ]);
+});
